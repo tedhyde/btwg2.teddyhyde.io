379eabb06047d9b99345c32f1ec3129489392406
Chris Dawson <xrdawson@gmail.com>
2018-10-16 23:23:00 -0700

    Fix broken build systme

--- edit.js
+++ edit.js
@@ -232,6 +232,5 @@ function setDisplayById( id, display ) {
 function revealContent(){
   setDisplayById("excerpt", "none" );
   setDisplayById("content", "inline-block" );
-  setTimeout( () => { window.scrollTo(0,0) }, 1000 );
 }
 
